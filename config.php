618884e35d44c3125a032fb9eab0a4d3bc6810bc
KunnAOV <killervn08042006@gmail.com>
2025-03-26 18:13:51 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 define('VERIFY_TOKEN', 'kunnaov_bot');
-define('ACCESS_TOKEN', 'YOUR_FACEBOOK_PAGE_ACCESS_TOKEN');
+define('ACCESS_TOKEN', 'EAAYFMbxPnyMBOzOBDu0FLSjfGtLvBwj8GE2qPab8oshNJh4FCoiMdRZCSmjwzVpNJYmAZBZBsXepsjRFpeAlonUJ6q6XMBpiWXrJOWYXhOpv0pLXjVZA862FWAFIJPWRRk8Vu5qaX3ZAAI9szcbZA9ptGTMlxZA2sRjEwRJPgwMUkc3kbpbyycLZBzByzCGNlmjsZBwZDZD');
 $admin = "Kunn AOV";
 $contact = "Zalo: 0123456789";
 ?>
